a8b92b4a22af9a0508e82f78263aa1a937458b1e
dathewolf <manuel.dahmen@gmx.com>
2021-10-07 13:06:05 +0200

    Create button "effect" on image list view.

---
+++ app/src/main/java/com/android/example/cameraxbasic/fragments/EffectsFragment.kt
@@ -0,0 +1,51 @@
+package com.android.example.cameraxbasic.fragments
+
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.android.example.cameraxbasic.R
+import one.empty3.Main
+import one.empty3.io.ProcessFile
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EffectsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EffectsFragment : AppCompatActivity() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var  effectList : ArrayList<ProcessFile>
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.select_effects)
+        effectList = Main.initListProcesses()
+        val l : List<String> = List<String>(effectList.size, init = {
+            effectList[it].toString()
+        })
+        val adapter : ArrayAdapter<String> = ArrayAdapter<String>(this,
+            android.R.layout.select_dialog_item, l)
+        val auto : AutoCompleteTextView = findViewById(R.id.effectsAutoCompleteTextView)
+        auto.setThreshold(1)
+        auto.setAdapter(adapter)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/android/example/cameraxbasic/model/ImageItemModel.kt
@@ -0,0 +1,27 @@
+package com.android.example.cameraxbasic.model;
+
+import android.content.ClipData
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import one.empty3.io.ProcessFile
+
+public class ImageItemModel : ViewModel() {
+        private val mutableSelectedItem = MutableLiveData<ClipData.Item>()
+        val selectedItem: LiveData<ClipData.Item> get() = mutableSelectedItem
+
+        fun selectItem(item: ClipData.Item) {
+            mutableSelectedItem.value = item
+        }
+
+    fun selectItem(item: ImageItemModel) {
+        TODO("Not yet implemented")
+    }
+    fun setEffect(effects : List<ProcessFile> ) {
+
+    }
+    fun applyEffect() {
+
+    }
+}
+
