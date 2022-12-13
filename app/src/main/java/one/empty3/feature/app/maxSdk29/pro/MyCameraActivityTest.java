853457019581a43e158cfd9b698b1aedd32cc516
manuel dahmen <manuel.dahmen@gmx.com>
2022-12-13 11:00:46 +0100

    Still working on it.

---
+++ app/src/main/java/one/empty3/feature/app/maxSdk29/pro/MyCameraActivityTest.java
@@ -0,0 +1,14 @@
+package one.empty3.feature.app.maxSdk29.pro;
+
+
+/*
+@RunWith(AndroidJUnit4::class)
+class MyTestSuite {
+    @Test
+    fun testEvent() {
+        launchActivity<MyActivity>().use {
+        }
+    }
+}
+
+ */
