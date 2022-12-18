f47578817f2dd1396d241ced6bee227611dcb544
manuel dahmen <manuel.dahmen@gmx.com>
2022-12-18 22:06:30 +0100

    starting New version

--- app/src/main/java/one/empty3/feature/app/maxSdk29/pro/MyFileProvider.java
+++ app/src/main/java/one/empty3/feature/app/maxSdk29/pro/MyFileProvider.java
@@ -2,7 +2,7 @@ package one.empty3.feature.app.maxSdk29.pro;
 
 import androidx.core.content.FileProvider;
 
-class MyFileProvider extends FileProvider {
+public class MyFileProvider extends FileProvider {
     public MyFileProvider() {
         super(R.xml.file_paths);
     }
