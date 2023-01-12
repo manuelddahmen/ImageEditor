dc2527e47dfbdf9d9c254a5fa86f369b59f28c4f
manuel dahmen <manuel.dahmen@gmx.com>
2023-01-12 09:44:57 +0100

    Update copyright Rename LICENSE.TXT

--- app/src/main/java/one/empty3/feature/app/process/ProcessDetailsActivity.kt
+++ app/src/main/java/one/empty3/feature/app/process/ProcessDetailsActivity.kt
@@ -25,14 +25,20 @@ import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import one.empty3.io.ProcessFile
 
 class ProcessDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContent {
-            Text("ZHello, World!")
-        }
+        setContent { HelloWorld() }
+    }
+
+    @Composable
+    @Preview
+    fun HelloWorld() {
+        Text("Hello, World!")
     }
 
     companion object {
