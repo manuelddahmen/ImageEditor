ea97c43a7530484e0d9962827b052bdf1bc412f5
manuel dahmen <manuel.dahmen@gmx.com>
2023-01-13 19:40:30 +0100

    Update copyright Rename LICENSE.TXT

--- app/src/main/java/one/empty3/feature/app/maxSdk29/pro/ui/theme/Color.kt
+++ app/src/main/java/one/empty3/feature/app/maxSdk29/pro/ui/theme/Color.kt
@@ -22,7 +22,7 @@ package one.empty3.feature.app.maxSdk29.pro.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Blue200 = Color(0xFF0099FF)
+val Blue500 = Color(0xFF00AAFF)
+val Blue700 = Color(0xFF0000FF)
 val Teal200 = Color(0xFF03DAC5)
