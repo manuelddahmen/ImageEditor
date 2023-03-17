71523fe48e46ec08159762e12c96e5e305e0a03e
manuel dahmen <manuel.dahmen@gmx.com>
2023-03-17 16:47:29 +0100

    Try on Android 13 Bug: show image in ImageViewSelection Try to put all ImageViewSelection setImageBitmap2() on UI Thread.

--- app/src/main/java/one/empty3/feature/app/maxSdk29/pro/FormulaStore.kt
+++ app/src/main/java/one/empty3/feature/app/maxSdk29/pro/FormulaStore.kt
@@ -30,7 +30,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import one.empty3.apps.simplecalculator.ui.theme.SimpleCalculatorTheme
+import one.empty3.apps.test.ui.theme.SimpleCalculatorTheme
 
 class FormulaStore : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
