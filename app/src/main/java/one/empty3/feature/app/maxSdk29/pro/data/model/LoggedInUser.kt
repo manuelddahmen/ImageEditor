4ce50dcb3f1a8e3cd3b9b56e4e15e829c13baa0b
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2023-12-19 22:44:20 +0100

    Update deps, sdk, gradle. Remove compose.

--- app/src/main/java/one/empty3/feature/app/maxSdk29/pro/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package one.empty3.feature.app.maxSdk29.pro.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
